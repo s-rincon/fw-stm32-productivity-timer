ad081a3ca8bdfb9e313d4058650499a9c81ded4f
Santiago Rincon <santiago.rincon@titoma.com>
2025-10-28 15:49:32 -0500

    feat(protimer): define the state machine handler.
    - Define the state machine handler with nested switch method evaluating first state and then event.
    
    - Define the prototypes for each state handler.

--- Core/Src/FSM/protimer.c
+++ Core/Src/FSM/protimer.c
@@ -1,1 +1,70 @@
 #include "protimer.h"
+
+static void protimer_idle_state_handler(protimer_t * const mobj, const protimer_event_t * const evt) {
+
+}
+
+static void protimer_time_set_state_handler(protimer_t * const mobj, const protimer_event_t * const evt) {
+
+}
+
+static void protimer_countdown_state_handler(protimer_t * const mobj, const protimer_event_t * const evt) {
+
+}
+
+static void protimer_pause_state_handler(protimer_t * const mobj, const protimer_event_t * const evt) {
+
+}
+
+static void protimer_stats_state_handler(protimer_t * const mobj, const protimer_event_t * const evt) {
+
+}
+
+static void protimer_state_machine(protimer_t * const mobj, const protimer_event_t * const evt) {
+    if ((mobj == NULL) || (evt == NULL)) {
+        return;
+    }
+
+    switch (mobj->active_state) {
+        case PROTIMER_STATE_IDLE:
+            protimer_idle_state_handler(mobj, evt);
+            break;
+
+        case PROTIMER_STATE_TIME_SET:
+            protimer_time_set_state_handler(mobj, evt);
+            break;
+
+        case PROTIMER_STATE_COUNTDOWN:
+            protimer_countdown_state_handler(mobj, evt);
+            break;
+
+        case PROTIMER_STATE_PAUSE:
+            protimer_pause_state_handler(mobj, evt);
+            break;
+
+        case PROTIMER_STATE_STATS:
+            protimer_stats_state_handler(mobj, evt);
+            break;
+
+        default:
+            // Unknown state, reset to IDLE
+            mobj->active_state = PROTIMER_STATE_IDLE;
+            break;
+    }
+    
+}
+
+void protimer_dispatch(protimer_t * const mobj, const protimer_event_t * const evt) {
+    if ((mobj == NULL) || (evt == NULL)) {
+        return;
+    }
+    // Dispatch the event to the state machine and handle transition if any
+}
+
+void protimer_init(protimer_t * const mobj) {
+    if (mobj == NULL) {
+        return;
+    }
+
+    // Initialize the protimer and handle initial transition to IDLE state
+}
